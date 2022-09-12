e83dfbc6a6ff20537098fe09d5adf177cefd4014
JinGyuGwak <wlsrb2695@naver.com>
2022-09-12 19:23:39 +0900

    자바로구현한 스프링프레임워크

--- src/main/java/com/zerobase/convpay/service/ConveniencePayService.java
+++ src/main/java/com/zerobase/convpay/service/ConveniencePayService.java
@@ -3,24 +3,37 @@ package com.zerobase.convpay.service;
 import com.zerobase.convpay.dto.*;
 import com.zerobase.convpay.type.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class ConveniencePayService {  //편결이 몸통 부분
-    private final MoneyAdapter moneyAdapter = new MoneyAdapter();
-    private final CardAdapter cardAdapter = new CardAdapter();
-    private final DiscountInterface discountInterface = new DiscountByPayMethod();
-    
+    private final Map<PayMethodType,PaymentInterface> paymentInterfaceMap =
+            new HashMap<>();
+    private final DiscountInterface discountInterface;
+
+    public ConveniencePayService(Set<PaymentInterface> paymentInterfaceset,
+                                 DiscountInterface discountInterface) {
+        paymentInterfaceset.forEach(
+                paymentInterface -> paymentInterfaceMap.put(
+                        paymentInterface.getPayMethodType(),
+                        paymentInterface
+                )
+        );
+
+        this.discountInterface = discountInterface;
+    }
+    //    private final MoneyAdapter moneyAdapter = new MoneyAdapter();
+//    private final CardAdapter cardAdapter = new CardAdapter();
+//    private final DiscountInterface discountInterface = new DiscountByPayMethod();
     //private final DiscountInterface discountInterface = new DiscountAll();
     //private final DiscountInterface discountInterface = new DiscountByConvenience();
     //private final PointAdapter pointAdapter = new PointAdater();
     // dip를 만족하면 이렇게 새로 추가 되어도 밑에 조건문에 하나만 추가하면 됨
 
-
     public PayResponse pay(PayRequest payRequest){
-        PaymentInterface paymentInterface;
-        if(payRequest.getPayMethodType() == PayMethodType.CARD){
-            paymentInterface=cardAdapter;
-        }else{
-            paymentInterface=moneyAdapter;
-        }
+        PaymentInterface paymentInterface = paymentInterfaceMap.get(payRequest.getPayMethodType());
+
 
         Integer discountedAmount = discountInterface.getDiscountedAmount(payRequest);
         PaymentResult payment = paymentInterface.payment(discountedAmount);
@@ -45,14 +58,7 @@ public class ConveniencePayService {  //편결이 몸통 부분
         return new PayResponse(PayResult.SUCCESS, discountedAmount);
     }
     public PayCancelResponse payCancel(PayCancelRequest payCancelRequest){
-
-        PaymentInterface paymentInterface;
-
-        if(payCancelRequest.getPayMethodType() == PayMethodType.CARD){
-            paymentInterface=cardAdapter;
-        }else{
-            paymentInterface=moneyAdapter;
-        }
+        PaymentInterface paymentInterface = paymentInterfaceMap.get(payCancelRequest.getPayMethodType());
 
         CancelPaymentResult cancelPaymentResult
                 = paymentInterface.cancelPayment(payCancelRequest.getPayCancelAmount());
